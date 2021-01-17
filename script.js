07f773f15787c96d8de60bd6388cc1100a73a15c
Nakula Marvellio <nakulamarvellio@gmail.com>
2021-01-18 00:54:49 +0700

    Add: final challenge 1 with js and css

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// alert("hello");
-console.log("hello");
+// challange 1: your age in days
+
+// using prompt to get data form user
+function ageInDays() {
+  var birthYear = prompt("What year were you born..my Friend?");
+  var ageInDays = (2021 - birthYear) * 365;
+  //   insert to the hmtl for ageindays result
+  var h1 = document.createElement("h1");
+  var textAnswer = document.createTextNode(
+    "You are " + ageInDays + " days old."
+  );
+  h1.setAttribute("id", "ageInDays");
+  h1.appendChild(textAnswer);
+  document.getElementById("flex-box-result").appendChild(h1);
+}
+
+// reset fucntion
+function reset() {
+  document.getElementById("ageInDays").remove();
+}
